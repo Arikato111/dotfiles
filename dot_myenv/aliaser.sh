9fa5126c9e93d048fd00b36e9d0c35a78a5b87b2
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-04-30 21:51:26 +0700

    create alias file

---
+++ dot_myenv/aliaser.sh
@@ -0,0 +1,4 @@
+alias dk-start="sudo systemctl start docker.socket && sudo systemctl start docker.service"
+alias dk-close="sudo systemctl stop docker.socket && sudo systemctl stop docker.service"
+alias mg-start="sudo systemctl start mongod"
+alias mg-close="sudo systemctl stop mongod"
