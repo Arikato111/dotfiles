27e5a28bcdd7668aee4d19e27848e8cb60725320
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-08-29 20:31:21 +0700

    fix config mason

--- dot_config/nvim/lua/plugins/example.lua
+++ dot_config/nvim/lua/plugins/example.lua
@@ -1,22 +1,8 @@
-
 return {
   -- disable conform
   -- because most nvim is not 0.10.+ yet
-   {
-   "stevearc/conform.nvim",
-    cond = false,
-  },
-  -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "cssls",
-      },
-    },
+    "stevearc/conform.nvim",
+    cond = false,
   },
 }
