d519d94dbf2537829203dc45dd1da154ca51e8af
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-11-08 16:05:29 +0700

    add command starting docker

---
+++ dot_zshrc.d/linux-docker.sh
@@ -0,0 +1,5 @@
+alias archlinux:docker='docker run -it --rm --network=host --name archlinux archlinux'
+alias debian:docker='docker run -it --rm --network=host --name debian debian'
+alias kali:docker='docker run -it --rm --network=host --name kali kalilinux/kali-rolling'
+alias ubuntu:docker='docker run -it --rm --network=host --name ubuntu ubuntu'
+
